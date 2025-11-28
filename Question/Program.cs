bec8b19148aa083debc0f9c9cb5f09c031137e34
LucasDev44 <lucas.dev44@gmail.com>
2025-11-28 01:07:08 -0300

    Add project files.

---
+++ Question/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.Write("Valor da fatura: ");
+decimal valor = decimal.Parse(Console.ReadLine());
+
+Console.Write("Vencimento (dd/mm/aaaa): ");
+DateTime venc = DateTime.Parse(Console.ReadLine());
+
+int dias = (DateTime.Today - venc).Days;
+
+if (dias <= 0)
+{
+    Console.WriteLine("Sem atraso.");
+    return;
+}
+
+decimal juros = valor * 0.025m * dias;
+Console.WriteLine($"Total de juros: R$ {juros:F2}");
+Console.WriteLine($"Total a pagar: R$ {(valor + juros):F2}");
